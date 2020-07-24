8135a06c0d344fef9f7e1bb46d6a53474fcb1a3f
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-25 01:06:55 +0530

    [Vaidehi]ADD:get maximum integer number

---
+++ Generics/Generics/TestMaximumUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class TestMaximumUtility
+    {
+        public int findMaximum(int firstNumber,int secondNumber ,int thirdNumber)
+        {
+            if(firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                return firstNumber;
+            }
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return thirdNumber;
+            }
+        }
+    }
+}
