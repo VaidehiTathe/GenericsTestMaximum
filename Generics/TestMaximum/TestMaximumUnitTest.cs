ce655b9ecf50d6d367363eac7a6621d07982c4d7
Vaidehi Tathe <vaidehitathe30@gmail.com>
2020-07-27 02:25:26 +0530

    [Vaidehi]ADD:testing multiple parameters

--- Generics/TestMaximum/TestMaximumUnitTest.cs
+++ Generics/TestMaximum/TestMaximumUnitTest.cs
@@ -7,72 +7,72 @@ namespace TestMaximum
         [Test]
         public void GivenMaxNumber_WhenAtFirstPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>(6, 5, 4);
-            int maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>();
+            int maximumNumber = testMax.findMaximum(6, 5, 4, 3, 2, 1);
             Assert.AreEqual(6,maximumNumber);
         }
 
         [Test]
         public void GivenMaxNumber_WhenAtSecondPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>(5,6,4);
-            int maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>();
+            int maximumNumber = testMax.findMaximum(5, 6, 4,3,2,1);
             Assert.AreEqual(6, maximumNumber);
         }
 
         [Test]
         public void GivenMaxNumber_WhenAtThirdPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>(5, 4, 6);
-            int maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<int> testMax = new Generics.TestMaximumUtility<int>();
+            int maximumNumber = testMax.findMaximum(5, 4, 6,3,2,1);
             Assert.AreEqual(6, maximumNumber);
         }
 
         [Test]
         public void GivenFloatNumber_WhenAtFirstPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>(6.0f, 5.1f, 2.1f);
-            float maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>();
+            float maximumNumber = testMax.findMaximum(6.0f, 5.1f, 2.1f,1.1f,1.2f);
             Assert.AreEqual(6.0f, maximumNumber);
         }
 
         [Test]
         public void GivenFloatNumber_WhenAtSecondPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>(5.1f, 6.2f, 4.2f);
-            float maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>();
+            float maximumNumber = testMax.findMaximum(5.1f, 6.2f, 4.2f,1.1f,2.1f);
             Assert.AreEqual(6.2f, maximumNumber);
         }
 
         [Test]
         public void GivenFloatNumber_WhenAtThirdPosition_ShouldReturnTrue()
         {
-            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>(5.1f, 4.2f, 6.1f);
-            float maximumNumber = testMax.findMaximum();
+            Generics.TestMaximumUtility<float> testMax = new Generics.TestMaximumUtility<float>();
+            float maximumNumber = testMax.findMaximum(5.1f, 4.2f, 6.1f,1.1f,1.2f,1.3f);
             Assert.AreEqual(6.1f, maximumNumber);
         }
 
         [Test]
         public void GivenString_WhenAtFirstPosition_ShouldReturnSameString()
         {
-            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>("Pineapple","Apple", "Banana");
-            string maximumString = testMax.findMaximum();
+            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>();
+            string maximumString = testMax.findMaximum("Pineapple", "Apple", "Banana","Mango");
             Assert.AreEqual("Pineapple", maximumString);
         }
 
         [Test]
         public void GivenString_WhenAtSecondPosition_ShouldReturnSameString()
         {
-            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>("Apple", "Pineapple", "Banana");
-            string maximumString = testMax.findMaximum();
+            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>();
+            string maximumString = testMax.findMaximum("Apple", "Pineapple", "Banana","Mango");
             Assert.AreEqual("Pineapple", maximumString);
         }
 
         [Test]
         public void GivenString_WhenAtThirdPosition_ShouldReturnSameString()
         {
-            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>("Apple", "Banana", "Pineapple");
-            string maximumString = testMax.findMaximum();
+            Generics.TestMaximumUtility<string> testMax = new Generics.TestMaximumUtility<string>();
+            string maximumString = testMax.findMaximum("Apple", "Banana", "Pineapple","Mango");
             Assert.AreEqual("Pineapple", maximumString);
         }
     }
